fb984c1f27a8cb7a1c05af1bc6a343f6ed2711b0
Zachariah Chow <zachariahchow@gmail.com>
2020-08-29 02:57:09 +0800

    feat: Add run predictions function

--- brain.py
+++ brain.py
@@ -7,3 +7,8 @@ prediction.setModelTypeAsSqueezeNet()
 prediction.setModelPath(os.path.join(
     execution_path, "squeezenet_weights_tf_dim_ordering_tf_kernels.h5"))
 prediction.loadModel()
+
+predictions, probabilities = prediction.predictImage(
+    os.path.join(execution_path, "images/baby.jpg"), result_count=5)
+for eachPrediction, eachProbability in zip(predictions, probabilities):
+    print(eachPrediction, " : ", eachProbability)
